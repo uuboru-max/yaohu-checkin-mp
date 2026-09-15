06d1d7f9c8ec64b11fbca5097764f83b4a72482a
uuboru-max <uuboru@gmail.com>
2026-09-15 12:49:25 +0000

    Add English vocab card on Me tab

--- pages/me/me.js
+++ pages/me/me.js
@@ -1,12 +1,23 @@
-const { COPY } = require("../../utils/data");
+const { COPY, VOCAB } = require("../../utils/data");
 const { load, save } = require("../../utils/store");
+
 Page({
-  data: { copy: COPY.zh, profile: "", locale: "zh" },
+  data: { copy: COPY.zh, profile: "", locale: "zh", vocab: VOCAB },
   onShow() {
     const state = load();
     const loc = state.locale === "en" ? "en" : "zh";
-    this.setData({ copy: COPY[loc], profile: state.profile, locale: loc });
+    this.setData({ copy: COPY[loc], profile: state.profile, locale: loc, vocab: VOCAB });
   },
-  setLang(e) { const state = load(); state.locale = e.currentTarget.dataset.lang; save(state); this.onShow(); },
-  onProfile(e) { const state = load(); state.profile = e.detail.value; save(state); this.setData({ profile: state.profile }); }
+  setLang(e) {
+    const state = load();
+    state.locale = e.currentTarget.dataset.lang;
+    save(state);
+    this.onShow();
+  },
+  onProfile(e) {
+    const state = load();
+    state.profile = e.detail.value;
+    save(state);
+    this.setData({ profile: state.profile });
+  },
 });
